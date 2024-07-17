810b53b956e851af0a9a882c78b9119ae1ba3fd8
Grant Ramsay <seapagan@gmail.com>
2024-07-17 15:20:41 +0100

    add an icon for a symlink
    Signed-off-by: Grant Ramsay <seapagan@gmail.com>

--- src/utils.rs
+++ src/utils.rs
@@ -19,7 +19,9 @@ pub fn get_file_name_with_slash(
 
 pub fn get_item_icon(metadata: &fs::Metadata) -> String {
     if metadata.is_dir() {
-        "\u{f07c}".to_string() // Placeholder icon
+        "\u{f07c}".to_string()
+    } else if metadata.is_symlink() {
+        "\u{f1177}".to_string()
     } else {
         "".to_string()
     }
