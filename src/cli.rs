0f72bee7013c723889c2e06ddd5769e63f208507
Grant Ramsay <seapagan@gmail.com>
2024-07-22 08:40:20 +0100

    move CLI code out to a module
    Signed-off-by: Grant Ramsay <seapagan@gmail.com>

---
+++ src/cli.rs
@@ -0,0 +1,73 @@
+// Set up the CLI arguments
+use clap::{Arg, ArgAction, Parser};
+
+#[derive(Parser)]
+#[command(
+    name = "lsplus",
+    author = env!("CARGO_PKG_AUTHORS"),
+    about =env!("CARGO_PKG_DESCRIPTION"),
+    long_about = None,
+    disable_help_flag = true,
+    arg(
+        Arg::new("help")
+            .long("help")
+            .action(ArgAction::Help)
+            .help("Print help information")
+    )
+)]
+pub struct CLI {
+    #[arg(short ='a', long = "all", action = ArgAction::SetTrue, help = "Do not ignore entries starting with .")]
+    pub show_all: bool,
+
+    #[arg(short ='A', long = "almost-all", action = ArgAction::SetTrue, help = "Do not list implied . and ..")]
+    pub almost_all: bool,
+
+    #[arg(short='l', long="long", action = ArgAction::SetTrue, help = "Display detailed information")]
+    pub long: bool,
+
+    #[arg(short='h', long="human-readable", action = ArgAction::SetTrue, help = "with -l, print sizes like 1K 234M 2G etc.")]
+    pub human_readable: bool,
+
+    #[arg(default_value = ".", help = "The path to list")]
+    pub path: String,
+
+    #[arg(short = 'p', long = "slash-dirs", action = ArgAction::SetTrue, help = "Append a slash to directories")]
+    pub slash: bool,
+
+    #[arg(short = 'D', long = "sort-dirs", action = ArgAction::SetTrue, help = "Sort directories first")]
+    pub dirs_first: bool,
+
+    #[arg(
+        long = "version",
+        short = 'V',
+        action = ArgAction::SetTrue,
+        help = "Print version information and exit",
+        global = true
+    )]
+    pub version: bool,
+}
+
+pub fn version_info() -> String {
+    let version = env!("CARGO_PKG_VERSION");
+    let authors = env!("CARGO_PKG_AUTHORS");
+    let description = env!("CARGO_PKG_DESCRIPTION");
+
+    // Provide default values if fields are empty
+    let authors = if authors.is_empty() {
+        "Unknown"
+    } else {
+        authors
+    };
+    let description = if description.is_empty() {
+        "No description provided"
+    } else {
+        description
+    };
+
+    format!(
+        "lsplus v{}\n\
+        \n{}\n\
+        \nReleased under the MIT license by {}\n",
+        version, description, authors
+    )
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
-use clap::{Arg, ArgAction, Parser};
-mod utils;
+use clap::Parser;
 use inline_colorization::*;
 use prettytable::{Cell, Row};
 use std::fs;
@@ -7,81 +6,13 @@ use std::io;
 use std::path::PathBuf;
 use std::process::exit;
 
-fn version_info() -> String {
-    let version = env!("CARGO_PKG_VERSION");
-    let authors = env!("CARGO_PKG_AUTHORS");
-    let description = env!("CARGO_PKG_DESCRIPTION");
-
-    // Provide default values if fields are empty
-    let authors = if authors.is_empty() {
-        "Unknown"
-    } else {
-        authors
-    };
-    let description = if description.is_empty() {
-        "No description provided"
-    } else {
-        description
-    };
-
-    format!(
-        "lsplus v{}\n\
-        \n{}\n\
-        \nReleased under the MIT license by {}\n",
-        version, description, authors
-    )
-}
-
-// Set up the CLI arguments
-#[derive(Parser)]
-#[command(
-    name = "lsplus",
-    author = env!("CARGO_PKG_AUTHORS"),
-    about =env!("CARGO_PKG_DESCRIPTION"),
-    long_about = None,
-    disable_help_flag = true,
-    arg(
-        Arg::new("help")
-            .long("help")
-            .action(ArgAction::Help)
-            .help("Print help information")
-    )
-)]
-struct Cli {
-    #[arg(short ='a', long = "all", action = ArgAction::SetTrue, help = "Do not ignore entries starting with .")]
-    show_all: bool,
-
-    #[arg(short ='A', long = "almost-all", action = ArgAction::SetTrue, help = "Do not list implied . and ..")]
-    almost_all: bool,
-
-    #[arg(short='l', long="long", action = ArgAction::SetTrue, help = "Display detailed information")]
-    long: bool,
-
-    #[arg(short='h', long="human-readable", action = ArgAction::SetTrue, help = "with -l, print sizes like 1K 234M 2G etc.")]
-    human_readable: bool,
-
-    #[arg(default_value = ".", help = "The path to list")]
-    path: String,
-
-    #[arg(short = 'p', long = "slash-dirs", action = ArgAction::SetTrue, help = "Append a slash to directories")]
-    slash: bool,
-
-    #[arg(short = 'D', long = "sort-dirs", action = ArgAction::SetTrue, help = "Sort directories first")]
-    dirs_first: bool,
-    #[arg(
-        long = "version",
-        short = 'V',
-        action = ArgAction::SetTrue,
-        help = "Print version information and exit",
-        global = true
-    )]
-    version: bool,
-}
+mod cli;
+mod utils;
 
 fn main() -> io::Result<()> {
-    let args = Cli::parse();
+    let args = cli::CLI::parse();
     if args.version {
-        println!("{}", version_info());
+        println!("{}", cli::version_info());
         exit(0);
     }
     // read in the command line arguments
