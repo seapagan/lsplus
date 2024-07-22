b0f9c36101f59feab72e4fca661d899193915187
Grant Ramsay <seapagan@gmail.com>
2024-07-22 21:30:30 +0100

    rewrite the icon logic
    this allows very easy expansion and clearer code
    
    Signed-off-by: Grant Ramsay <seapagan@gmail.com>

--- src/utils/icons.rs
+++ src/utils/icons.rs
@@ -1,11 +1,34 @@
 use std::fs;
 
+use std::fmt;
+
+#[derive(Debug, Clone, Copy)]
+enum UnicodeChar {
+    Folder = '\u{f07c}' as isize,
+    Symlink = '\u{f1177}' as isize,
+
+    GenericFile = '\u{f15b}' as isize,
+}
+
+impl UnicodeChar {
+    fn as_char(self) -> char {
+        // This is safe because we know all our values are valid Unicode code points
+        char::from_u32(self as u32).unwrap()
+    }
+}
+
+impl fmt::Display for UnicodeChar {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.as_char())
+    }
+}
+
 pub fn get_item_icon(metadata: &fs::Metadata) -> String {
     if metadata.is_dir() {
-        "\u{f07c}".to_string()
+        UnicodeChar::Folder.to_string()
     } else if metadata.is_symlink() {
-        "\u{f1177}".to_string()
+        UnicodeChar::Symlink.to_string()
     } else {
-        "".to_string()
+        UnicodeChar::GenericFile.to_string()
     }
 }
